e26a8e63379a1f741794ceeba5874a5e1f7824de
pawlo1994 <63592537+pawlo1994@users.noreply.github.com>
2020-06-04 22:13:22 +0200

    Update song.js

--- js/song.js
+++ js/song.js
@@ -10,18 +10,24 @@ songButton1.addEventListener("click", () => {
     song.style.display = "flex";
     songHeader.textContent = "PoScriMus - In the C of Calmness";
     songVideo.src = "https://www.youtube.com/embed/LrAqEPDiUzA";
+    songVideo.addEventListener("load", () => {
+songVideo.style.display = "block";
 });
 
 songButton2.addEventListener("click", () => {
     song.style.display = "flex";
     songHeader.textContent = "PoScriMus - Hip-Hop";
     songVideo.src = "https://www.youtube.com/embed/sQL3kvhqtLM";
+songVideo.addEventListener("load", () => {
+songVideo.style.display = "block";
 });
 
 songButton3.addEventListener("click", () => {
     song.style.display = "flex";
     songHeader.textContent = "PoScriMus - Sweet Reggae";
     songVideo.src = "https://www.youtube.com/embed/inE9P9wqzzI";
+songVideo.addEventListener("load", () => {
+songVideo.style.display = "block";
 });
 
 closeButton.addEventListener("click", () => {

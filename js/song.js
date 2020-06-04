d1b96b8e6ef4a7a3190a34b442c2520b8deb37d2
pawlo1994 <pawlo4-94@o2.pl>
2020-06-04 19:37:24 +0200

    song.js updated

--- js/song.js
+++ js/song.js
@@ -1,28 +1,30 @@
-(() => {
-    const button_1 = document.querySelector(".js-song__button-1");
-    const button_2 = document.querySelector(".js-song__button-2");
-    const button_3 = document.querySelector(".js-song__button-3");
-    const song_1 = document.querySelector(".js-song-1");
-    const song_2 = document.querySelector(".js-song-2");
-    const song_3 = document.querySelector(".js-song-3");
+const songButton1 = document.querySelector(".js-song__button-1");
+const songButton2 = document.querySelector(".js-song__button-2");
+const songButton3 = document.querySelector(".js-song__button-3");
+const closeButton = document.querySelector(".close__button");
+const song = document.querySelector(".song");
+const songHeader = document.querySelector(".song__header");
+const songVideo = document.querySelector(".song__video");
 
-    const toggleClass_1 = () => {
-        song_1.classList.toggle("song-1--open");
-    };
+songButton1.addEventListener("click", () => {
+    song.style.display = "flex";
+    songHeader.textContent = "PoScriMus - In the C of Calmness";
+    songVideo.src = "https://www.youtube.com/embed/LrAqEPDiUzA";
+});
 
-    const toggleClass_2 = () => {
-        song_2.classList.toggle("song-2--open");
-    };
+songButton2.addEventListener("click", () => {
+    song.style.display = "flex";
+    songHeader.textContent = "PoScriMus - Hip-Hop";
+    songVideo.src = "https://www.youtube.com/embed/sQL3kvhqtLM";
+});
 
-    const toggleClass_3 = () => {
-        song_3.classList.toggle("song-3--open");
-    };
+songButton3.addEventListener("click", () => {
+    song.style.display = "flex";
+    songHeader.textContent = "PoScriMus - Sweet Reggae";
+    songVideo.src = "https://www.youtube.com/embed/inE9P9wqzzI";
+});
 
-    button_1.addEventListener("click", toggleClass_1);
-    button_2.addEventListener("click", toggleClass_2);
-    button_3.addEventListener("click", toggleClass_3);
-    song_1.addEventListener("click", toggleClass_1);
-    song_2.addEventListener("click", toggleClass_2);
-    song_3.addEventListener("click", toggleClass_3);
-
-})();
+closeButton.addEventListener("click", () => {
+    song.style.display = "none";
+    songVideo.src = " ";
+})

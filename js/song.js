f7e8d2c10b7ea5339aaa6887891ff38a46582fc5
pawlo1994 <pawlo4-94@o2.pl>
2020-06-05 16:58:35 +0200

    new "loading" image for song__video

--- js/song.js
+++ js/song.js
@@ -26,5 +26,5 @@ songButton3.addEventListener("click", () => {
 
 closeButton.addEventListener("click", () => {
     song.style.display = "none";
-    songVideo.src = "images/songs.svg";
+    songVideo.src = "images/youtube.svg";
 })

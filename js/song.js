9b73eba7e42fd125de892a42292d6bd71c0fbe6d
pawlo1994 <pawlo4-94@o2.pl>
2020-11-29 19:53:09 +0100

    new actions for closeButton and resizeButton

--- js/song.js
+++ js/song.js
@@ -2,7 +2,9 @@ const songButton1 = document.querySelector(".js-song__button-1");
 const songButton2 = document.querySelector(".js-song__button-2");
 const songButton3 = document.querySelector(".js-song__button-3");
 const closeButton = document.querySelector(".close__button");
+const resizeButton = document.querySelector(".resize__button");
 const song = document.querySelector(".song");
+const songMini = document.querySelector(".song__mini");
 const songHeader = document.querySelector(".song__header");
 const songVideo = document.querySelector(".song__video");
 
@@ -25,6 +27,22 @@ songButton3.addEventListener("click", () => {
 });
 
 closeButton.addEventListener("click", () => {
+    if (song.classList.contains("song__mini")) {
+        song.classList.remove("song__mini");
+        song.classList.add("song");
+        resizeButton.innerHTML = "&darr;"
+    };
     song.style.display = "none";
     songVideo.src = "images/youtube.svg";
-})
+});
+resizeButton.addEventListener("click", () => {
+    if (song.classList.contains("song")) {
+        song.classList.remove("song");
+        song.classList.add("song__mini");
+        resizeButton.innerHTML = "&uarr;";
+    } else {
+        song.classList.remove("song__mini");
+        song.classList.add("song");
+        resizeButton.innerHTML = "&darr;";
+    }
+});

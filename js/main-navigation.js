148a62d26a20090a986f5eb48e23eaf05f5793ef
pawlo1994 <pawlo4-94@o2.pl>
2020-11-30 11:40:45 +0100

    hiding menu during scroll added

--- js/main-navigation.js
+++ js/main-navigation.js
@@ -9,4 +9,10 @@
 
     button.addEventListener("click", toggleClass);
     menu.addEventListener("click", toggleClass);
+    document.addEventListener("scroll", () => {
+        if (menu.classList.contains("navigation-menu--open") && (button.classList.contains("navigation-menu__mobile-button--open"))) {
+            button.classList.remove("navigation-menu__mobile-button--open");
+            menu.classList.remove("navigation-menu--open");
+        };
+    });
 })();

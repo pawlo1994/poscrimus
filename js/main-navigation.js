a8a78ab20b8814d6743ce96daafe2ba1d4503603
pawlo1994 <pawlo4-94@o2.pl>
2021-03-11 19:55:01 +0100

    semi-colon added

--- js/main-navigation.js
+++ js/main-navigation.js
@@ -1,6 +1,6 @@
 (() => {
     const button = document.querySelector(".js-navigation-menu__mobile-button");
-    const menu = document.querySelector(".js-navigation-menu")
+    const menu = document.querySelector(".js-navigation-menu");
 
     const toggleClass = () => {
         menu.classList.toggle("navigation-menu--open");

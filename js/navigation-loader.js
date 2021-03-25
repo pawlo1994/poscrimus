4c223c9f32ced92a318d3639274ac61f6d4e66b7
pawlo4-94 <pawlo4-94@o2.pl>
2021-03-25 23:53:52 +0100

    navigation loader js file deleted

--- js/navigation-loader.js
+++
@@ -1,3 +0,0 @@
-const navigation__loader = document.querySelector(".navigation__loader");
-const banner = document.querySelector(".banner");
-banner.addEventListener("load", navigation__loader.style.display = "none");

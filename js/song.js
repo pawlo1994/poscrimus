0300bb96151ded9ae02939a03195c7ff48cc2c27
pawlo4-94 <pawlo4-94@o2.pl>
2021-04-10 15:05:18 +0200

    variables for resize button and song box deleted

--- js/song.js
+++ js/song.js
@@ -2,71 +2,35 @@ const songBoxButton1 = document.querySelector(".js-song__button-1");
 const songBoxButton2 = document.querySelector(".js-song__button-2");
 const songBoxButton3 = document.querySelector(".js-song__button-3");
 const closeButton = document.querySelector(".close__button");
-const resizeButton = document.querySelector(".resize__button");
-const songBox = document.querySelector(".song-box");
+const songBoxMini = document.querySelector(".song-box-mini");
 const songBoxHeaderDescription = document.querySelector(".song-box__header__description");
 const songBoxVideo = document.querySelector(".song-box__video");
 
 songBoxButton1.addEventListener("click", () => {
-    if (songBox.classList.contains("song-box-mini")) {
-        songBox.classList.add("song-box-mini--open");
-        resizeButton.innerHTML = "&uarr;"
-    }
-    else {
-        songBox.classList.add("song-box--open");
-        resizeButton.innerHTML = "&darr;"
+    if (songBoxMini.classList.contains("song-box-mini")) {
+        songBoxMini.classList.add("song-box-mini--open");
     }
     songBoxHeaderDescription.textContent = "PoScriMus - In the C of Calmness";
     songBoxVideo.src = "https://www.youtube.com/embed/LrAqEPDiUzA";
 });
 
 songBoxButton2.addEventListener("click", () => {
-    if (songBox.classList.contains("song-box-mini")) {
-        songBox.classList.add("song-box-mini--open");
-        resizeButton.innerHTML = "&uarr;"
-    }
-    else {
-        songBox.classList.add("song-box--open");
-        resizeButton.innerHTML = "&darr;"
+    if (songBoxMini.classList.contains("song-box-mini")) {
+        songBoxMini.classList.add("song-box-mini--open");
     }
     songBoxHeaderDescription.textContent = "PoScriMus - Hip-Hop";
     songBoxVideo.src = "https://www.youtube.com/embed/sQL3kvhqtLM";
 });
 
 songBoxButton3.addEventListener("click", () => {
-    if (songBox.classList.contains("song-box-mini")) {
-        songBox.classList.add("song-box-mini--open");
-        resizeButton.innerHTML = "&uarr;"
-    }
-    else {
-        songBox.classList.add("song-box--open");
-        resizeButton.innerHTML = "&darr;"
+    if (songBoxMini.classList.contains("song-box-mini")) {
+        songBoxMini.classList.add("song-box-mini--open");
     }
     songBoxHeaderDescription.textContent = "PoScriMus - Sweet Reggae";
     songBoxVideo.src = "https://www.youtube.com/embed/inE9P9wqzzI";
 });
 
 closeButton.addEventListener("click", () => {
-    if (songBox.classList.contains("song-box")) {
-        songBox.classList.remove("song-box--open");
-    }
-    else {
-        songBox.classList.remove("song-box-mini--open");
-    }
+    songBoxMini.classList.toggle("song-box-mini--open");
     songBoxVideo.src = "images/youtube.svg";
-});
-resizeButton.addEventListener("click", () => {
-    if (songBox.classList.contains("song-box")) {
-        songBox.classList.remove("song-box");
-        songBox.classList.remove("song-box--open");
-        songBox.classList.add("song-box-mini");
-        songBox.classList.add("song-box-mini--open");
-        resizeButton.innerHTML = "&uarr;";
-    } else {
-        songBox.classList.remove("song-box-mini");
-        songBox.classList.remove("song-box-mini--open");
-        songBox.classList.add("song-box");
-        songBox.classList.add("song-box--open");
-        resizeButton.innerHTML = "&darr;";
-    }
 });
